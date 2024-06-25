f4f457cca9bbf07837b7dc2183679667d97e8920
vipos89 <1989146ViPoS>
2024-06-25 22:56:17 +0300

    cache lib

--- cache.go
+++ cache.go
@@ -1,1 +1,27 @@
 package memory_cash
+
+type Cache struct {
+	data map[string]interface{}
+}
+
+func NewCache() *Cache {
+	return &Cache{
+		data: make(map[string]interface{}),
+	}
+}
+
+func (c *Cache) Set(key string, value interface{}) {
+	c.data[key] = value
+}
+
+func (c *Cache) Get(key string) interface{} {
+	val, exists := c.data[key]
+	if !exists {
+		return nil
+	}
+	return val
+}
+
+func (c *Cache) Delete(key string) {
+	delete(c.data, key)
+}
